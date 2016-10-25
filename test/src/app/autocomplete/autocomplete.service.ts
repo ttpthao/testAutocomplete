5fc56329ac15d6bd8c5534dbca042193977fee71
Thao Tran Thi Phuong <phuongthao.tran@orientsoftware.net>
2016-10-25 11:51:32 +0700

    add service + param

--- test/src/app/autocomplete/autocomplete.service.ts
+++ test/src/app/autocomplete/autocomplete.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AutocompleteParam } from './autocompleteParam';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AutocompleteService {
+  public url: string = "";
+  private headers: Headers;
 
-  constructor() { }
+  constructor(private _http: Http, private _param: AutocompleteParam) {
+    this.url = _param.url;
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Accept', 'application/json');
+  }
 
+  getAutocomplete() {
+    return this._http.get(this.url);
+  }
 }
--- test/src/app/autocomplete/autocompleteParam.ts
+++ test/src/app/autocomplete/autocompleteParam.ts
@@ -1,5 +1,18 @@
-export class autocompleteParam {
+export class AutocompleteParam {
     placeholder: string;
     url: string;
     hasGroupTitle: boolean;
-};
+}
+
+// export class AutocompleteParam implements IAutocomplete {
+//     placeholder: string;
+//     url: string;
+//     hasGroupTitle: boolean;
+
+//     constructor(param?: IAutocomplete){
+//         if (!param) return;
+//         this.placeholder = param.placeholder;
+//         this.url = param.url;
+//         this.hasGroupTitle = param.hasGroupTitle;
+//     }
+// }
